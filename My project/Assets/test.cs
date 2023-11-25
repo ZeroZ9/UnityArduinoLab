c1af55aa4c6db738b9816e54e4c5f25ca38265e6
ZeroZ9 <tranlonguuu@gmail.com>
2023-11-25 16:13:57 -0500

    add the touching and moving the cube behavior to the arm in test cs

--- My project/Assets/test.cs
+++ My project/Assets/test.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.IO.Ports;
 using UnityEngine;
 
-public class RotateThroughtSerial : MonoBehaviour
+public class RotateThroughSerial : MonoBehaviour
 {
     SerialPort data_stream = new SerialPort("/dev/cu.usbmodem123456781", 115200); // Replace with your port name
     public string receivedstring;
-    public GameObject test_data;
+    public GameObject arm;
 
     private GameObject objectToHold;
-
     private bool isHolding = false;
+    private bool isPointingToSphere = false; // New variable to track whether the arm is pointing to the sphere
 
     void Start()
     {
@@ -42,9 +42,9 @@ public class RotateThroughtSerial : MonoBehaviour
                     float y = float.Parse(data_array[2]);
                     float z = float.Parse(data_array[3]);
 
-                    test_data.transform.Rotate(new Vector3(x, y, z) * Time.deltaTime);
+                    arm.transform.Rotate(new Vector3(x, y, z) * Time.deltaTime);
 
-                    if (command == "hold" && !isHolding)
+                    if (command == "hold" && !isHolding && isPointingToSphere)
                     {
                         isHolding = true;
                         HoldObject();
@@ -61,16 +61,79 @@ public class RotateThroughtSerial : MonoBehaviour
                 Debug.LogError("Error reading from serial port: " + e.Message);
             }
         }
+
+        MoveArmWithKeyboard();
+    } 
+
+
+    // detect the arm is touching to the cube
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == objectToHold)
+        {
+            isPointingToSphere = true;
+        }
     }
 
+    // detect the arm is not touching to the cube
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == objectToHold)
+        {
+            isPointingToSphere = false;
+        }
+    }
+
+    // Move the arm around with the cube
     void HoldObject()
     {
-        objectToHold.transform.SetParent(test_data.transform, false);
+        objectToHold.transform.SetParent(arm.transform, false);
+        objectToHold.transform.localPosition = Vector3.zero; // Adjust if necessary
+        objectToHold.transform.localRotation = Quaternion.identity; // Adjust if necessary
     }
 
+    // release the cube
     void ReleaseObject()
     {
         objectToHold.transform.SetParent(null, false);
-        objectToHold.transform.position = new Vector3(0, 0, 0); // Optionally reset position
+        // Optionally, you can reset the position and rotation to some default state
+        objectToHold.transform.position = new Vector3(0, 0, 0);
+        objectToHold.transform.rotation = Quaternion.identity;
+    }
+
+    void MoveArmWithKeyboard()
+    {
+        float moveSpeed = 5.0f; // Speed of the movement, adjust as needed
+        float rotationSpeed = 100.0f; // Speed of the rotation, adjust as needed
+
+        // Move the arm forward and backward with W and S
+        if (Input.GetKey(KeyCode.W))
+        {
+            arm.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            arm.transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        }
+
+        // Move the arm left and right with A and D
+        if (Input.GetKey(KeyCode.A))
+        {
+            arm.transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            arm.transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+
+        // Rotate the arm with Q and E
+        if (Input.GetKey(KeyCode.Q))
+        {
+            arm.transform.Rotate(Vector3.down * rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            arm.transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        }
     }
 }
